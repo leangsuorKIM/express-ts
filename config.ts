5aaa44ec71d61a8e76cdf2a63b7cf3c04280f98a
Leangsuor <leangsourkim098@gmail.com>
2024-05-22 14:00:49 +0700

    update config

--- config.ts
+++ config.ts
@@ -6,5 +6,5 @@ dotenv.config();
 export const API_KEY: string = process.env.API_KEY || '';
 export const NODE_ENV: string = process.env.NODE_ENV || 'development';
 
-export const PORT: number = parseInt(process.env.PORT || '8000', 10);
-export const DATABASE_URL: string = process.env.DATABASE_URL || '';
+export const PORT: number = parseInt(process.env.PORT || '8080', 10);
+export const DATABASE_URL: string = process.env.DATABASE_URL || 'mongodb+srv://nothank:nothank@cluster0.uchcnmz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
